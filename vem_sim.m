3cfe4bd185fb970f42dfaa151dfccf19c28dfc33
Ty Trusty <tytrusty@utexas.edu>
2020-11-10 13:26:00 -0600

    things are working for now...

--- vem_sim.m
+++ vem_sim.m
@@ -1,16 +1,19 @@
 function vem_sim
     dt = 0.01;      	% timestep
-    C = 0.5 * 17;   	% Lame parameter 1
-    D = 0.5 * 150;   	% Lame parameter 2
-    gravity = -100;
-    order = 2;          
+    C = 0.5 * 170;   	% Lame parameter 1
+    D = 0.5 * 1500;   	% Lame parameter 2
+    gravity = -500;
+    order = 2;
+    k_error = 10000;
+    rho = 100;
+    
     
    % Load mesh
     [V,F] = readOBJ('plane.obj');
     V = V(:,1:2);
     E = boundary_faces(F);
     
-    fig=figure(1)
+    fig=figure(1);
     clf;
     X_plot = plot(V(:,1),V(:,2),'.');
     hold on;
@@ -19,45 +22,45 @@ function vem_sim
     x0 = V(unique([E(:,1) E(:,2)]),:);
     x0 = x0(:,1:2)';
     min_I = find(x0(2,:) == max(x0(2,:)));
+    min_I = [2];
     x0 = [0 0; 0 2;  2 2; 2 0; ]';
     x = x0;
-    
-    g_force = repmat([0 gravity], size(x0,2),1)';
-    g_force = g_force(:);
-    
+    v = zeros(size(x));
+
     E_plot = plot([x(1,:) x(1,1)],[x(2,:) x(2,1)],'o','LineWidth',2, 'Color', 'm');
     axis equal
-    xlim([-1 3])
-    ylim([-.5 2.5])
+%     xlim([-1 3])
+    ylim([-2 2.5])
     
-%    P = fixed_point_constraint_matrix(x0', [2 3]')
-%      b = qt - P'*P*qt;
-%     qt = P*qt;
-%     vt = 0*qt;
-%     
+    % Constraint matrix for boundary conditions.
+	% P = fixed_point_constraint_matrix(x0', [2 3]');
+    P = fixed_point_constraint_matrix(x0',sort(min_I)');
+    
+    % Gravity force vector.
+  	f_gravity = repmat([0 gravity], size(x0,2),1)';
+    f_gravity = dt*P*f_gravity(:);
+    
     % Undeformed Center of mass
     x0_com = mean(x0,2);
-    plot(x0_com(1), x0_com(2), 'x');
     
     % Build Monomial bases
-    Q = x0 - x0_com;
+    Q0 = x0 - x0_com;
     if order == 2
-        Q_ = zeros(5, size(Q,2));
-        Q_(1:2,:) = Q;
-        Q_(3,:) = Q(1,:).^2;
-        Q_(4,:) = Q(2,:).^2;
-        Q_(5,:) = Q(1,:).*Q(2,:);
-        Q=Q_;
+        Q_ = zeros(5, size(Q0,2));
+        Q_(1:2,:) = Q0;
+        Q_(3,:) = Q0(1,:).^2;
+        Q_(4,:) = Q0(2,:).^2;
+        Q_(5,:) = Q0(1,:).*Q0(2,:);
+        Q0=Q_;
     end
     
     % Build 'B' matrices (Aqq in shape matching)
     % Single shape currently.
-    [SU, S, SV] = svd(Q*Q');
+    [SU, S, SV] = svd(Q0*Q0');
     S = diag(S);
     S = 1./ max(S, 1e-4);
-    B = Q' * (SV * diag(S) * SU');
+    B = Q0' * (SV * diag(S) * SU');
     
-
     % Build Monomial bases for all quadrature points
     Q = V' - x0_com;
     if order == 2
@@ -70,26 +73,17 @@ function vem_sim
     end
     
     % Computing inverted mass matrices
-    M = zeros(numel(x0),numel(x0));
-    rho = 100;
-    for i = 1:size(Q,2)
-        BM = B * Q(:,i);
-        M = M +  mass_matrix_n(BM);
+%     M = zeros(numel(x0),numel(x0));
+%     for i = 1:size(Q,2)
+%         BM = B * Q(:,i);
+% %         M = M +  mass_matrix_n(BM);
 %         M = M +  mass_matrix_36(BM);
-        
-    end
-    M = M * rho;
-    
-    M_inv = pinv(rho * M);
-    %M_inv2 = inv(rho * M);
-    v = zeros(size(x));
+%         
+%     end
+%     M = M * rho;
+    M = rho * eye(numel(x0));
+
     ii=1;
-    
-    M = 1e3 * eye(8);
-%     M_inv = 1e-3 * eye(8);
-    %project mass matrix down
-    %M_inv = P*M_inv*P';
-    
     for t=0:dt:30
         x_com = mean(x,2);
         
@@ -126,23 +120,26 @@ function vem_sim
             % Stiffness matrix
             d2V_dF2 = neohookean_dF2(F,C,D);
             K = K - dF_dq' * d2V_dF2 * dF_dq;
-                      
         end
-               
-%         a = -M_inv * dV_dq;
-%         a = reshape(a,2,[]);
-%         v = v + dt*a + dt*[0 gravity]';
-%         x = x + dt*v;
         
-        lhs = (M - dt*dt*K);
-        rhs = M*v(:) - dt*dV_dq + dt*g_force;
-        qdot = lhs \ rhs;
+        % Error correction force
+        f_error = A*Q0 + x_com - x;
+        f_error = k_error*(dt * P * f_error(:));
+        
+        % Force from potential energy.
+        f_internal = -dt*P*dV_dq;
+        
+        % Computing linearly-implicit velocity update
+        lhs = P*(M - dt*dt*K)*P';
+        rhs = P*M*v(:) + f_internal + f_gravity + f_error;
+        qdot = lhs \ rhs;   % perform solve
+        qdot = P'*qdot;     % unproject velocities
         v = reshape(qdot,2,[]);
+
+        % Update position
         x = x + dt*v;
         
-        x(:,2:3)= x0(:,2:3);
-        % x(:,min_I)= x0(:,min_I);
-        
+        % Update plot.
         E_plot.XData = [x(1,:) x(1,1)];
         E_plot.YData = [x(2,:) x(2,1)];
         % Different 'F' for each x, so this need to be in the loop
@@ -152,8 +149,8 @@ function vem_sim
         X_plot.YData = Points(2,:);
         drawnow
         
-%         fn=sprintf('output_png\\lin_%03d.png',ii)
-%         saveas(fig,fn);
-%         ii=ii+1;
+        fn=sprintf('output_png\\quad_%03d.png',ii)
+        saveas(fig,fn);
+        ii=ii+1;
     end
 end
