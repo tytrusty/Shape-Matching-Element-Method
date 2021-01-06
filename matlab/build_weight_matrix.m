55d9ac91f5a26f0fe95afd680e9ba5fa7d00f76c
Ty Trusty <tytrusty@utexas.edu>
2021-01-06 00:18:58 -0600

    Support for truncating weight matrix where weights are small. Improves stiffness computation performance.

--- matlab/build_weight_matrix.m
+++ matlab/build_weight_matrix.m
@@ -1,12 +1,44 @@
-function W = build_weight_matrix(w,d,k)
-    m = size(w,1); % number of points
+function [W,W_I,W_S] = build_weight_matrix(w, d, k, varargin)
+    p = inputParser;
+    addParameter(p, 'Truncate', true);
+    addParameter(p, 'Threshold', 1e-2);
+    parse(p,varargin{:});
+    
+    truncate = p.Results.Truncate;
+    threshold = p.Results.Threshold;
+    
+    m = size(w,1);  % number of points
     n = size(w,2);  % number of shapes
     
-    W = zeros(m, d*d, d*(k*n + 1));
+    W = cell(m,1);
+    W_I = cell(m,1);
+    W_S = cell(m,1);
+    
     for i = 1:m
-%         Wi = zeros(d*k, d*k*n + d);
-        for j = 1:n
-            W(i, :, d*k*(j-1)+1:d*k*j) = eye(d*k)*w(i,j);
-        end        
+        
+        if truncate
+            ToKeep = w(i,:) > threshold;
+        else
+            ToKeep = ones(1,n);
+        end
+        
+        % Shape indices for weights above the threshold.
+        W_I{i} = find(ToKeep);
+        t = numel(W_I{i});
+       
+        % Forming selection and weight matrices.
+        W{i} = zeros(d*k, d*k*t);
+        W_S{i} = sparse(zeros(d*k*t, d*(k*n + 1)));
+        for j = 1:t
+            I = W_I{i}(j);
+            
+            col_range = d*k*(j-1)+1:d*k*j; 
+            W{i}(:, col_range) = eye(d*k)*w(i,I);
+            
+            
+            row_range = d*k*(j-1)+1 : d*k*j;
+            col_range = d*k*(I-1)+1:d*k*I; 
+            W_S{i}(row_range, col_range) = eye(d*k);
+        end     
     end
 end
--- matlab/vem_dF_dc.m
+++ matlab/vem_dF_dc.m
@@ -1,11 +1,9 @@
-function dF_dc = vem_dF_dc(dM_dX, W, w, E, d, k)
-    m = size(w,1);
-    n = size(w,2);
+function dF_dc = vem_dF_dc(dM_dX, W)
+    m = size(dM_dX,1);
+    dF_dc = cell(m,1);
     
-    dF_dc = zeros(m, d*d, d*(k*n + 1));
     for i = 1:m
         dMi_dX = squeeze(dM_dX(i,:,:));
-        Wi = squeeze(W(i,:,:));
-        dF_dc(i,:,:) = dMi_dX * Wi;
+        dF_dc{i} = dMi_dX * W{i};
     end
 end
