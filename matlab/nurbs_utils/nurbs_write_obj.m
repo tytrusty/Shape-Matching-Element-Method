ed35f03f181b0ad7e8d3c6d52c370dc3e5993b2b
honglin-c <chl9797@live.com>
2021-01-10 22:05:11 -0500

    output vertex normals when outputting obj

--- matlab/nurbs_utils/nurbs_write_obj.m
+++ matlab/nurbs_utils/nurbs_write_obj.m
@@ -1,6 +1,7 @@
 function nurbs_write_obj(q, parts, path, ii)
     faces=[];
     verts=[];
+    normals = [];
 
     q_idx = 0;
     for i=1:numel(parts)
@@ -9,8 +10,10 @@ function nurbs_write_obj(q, parts, path, ii)
         xi = squeeze(sum(parts{i}.hires_J .* qi,1));
 
         faces_i = parts{i}.hires_T + size(verts,1);
+        normals_i = nurbs_normals(parts{i}.srf.nurbs, parts{i}.hires_UV, parts{i}.p);
         faces=[faces; faces_i];
         verts=[verts; xi'];
+        normals=[normals; normals_i];
         
         % obj_fn = "output/obj/part_" + i + "_" + int2str(ii) + ".obj";
         % writeOBJ(obj_fn, fvc.vertices, fvc.faces);
@@ -18,6 +21,6 @@ function nurbs_write_obj(q, parts, path, ii)
     end
     
     % you better have gptoolbox, son
-    writeOBJ(path, verts, faces);
+    writeOBJ(path, verts, faces, [], zeros(size(faces,1),3), normals, zeros(size(faces,1),3));
 end
 
