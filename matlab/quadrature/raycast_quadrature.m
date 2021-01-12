48cffc1d7d35971f1e991084b696e124a1943de5
Ty Trusty <tytrusty@utexas.edu>
2021-01-12 04:00:04 -0600

    fix raycasting quadrature volumes. They were wrong! Also fix sampling positions

--- matlab/quadrature/raycast_quadrature.m
+++ matlab/quadrature/raycast_quadrature.m
@@ -24,10 +24,15 @@ xrange = [OT.BinBoundaries(1,1) OT.BinBoundaries(1,4)];
 yrange = [OT.BinBoundaries(1,2) OT.BinBoundaries(1,5)];
 zrange = [OT.BinBoundaries(1,3) OT.BinBoundaries(1,6)];
 
+% TODO get rid of OT stuff :)
 
-[U, V] = meshgrid(linspace(yrange(1), yrange(2), ray_samples(1)), ...
-                  linspace(zrange(1), zrange(2), ray_samples(2)));
-UV = [U(:) V(:)];      
+half_inv_du = 1 / ray_samples(1) / 2;
+half_inv_dv = 1 / ray_samples(2) / 2;
+[U, V] = meshgrid(linspace(yrange(1), yrange(2), 1+ray_samples(1)), ...
+                  linspace(zrange(1), zrange(2), 1+ray_samples(2)));
+U = U(1:end-1, 1:end-1);
+V = V(1:end-1, 1:end-1);
+UV = [U(:)+half_inv_du V(:)+half_inv_dv];
 
 dA = ((yrange(2)-yrange(1))/ray_samples(1)) * ...
      ((zrange(2)-zrange(1))/ray_samples(2)); 
@@ -53,10 +58,15 @@ for i = 1:numel(hit_ids)
         dist = reshape(dist,2,[]);
         UV_rep = repelem(UV(idx,:),samples,1);
         for j = 1:size(dist,2)
-            x_samples = linspace(dist(1,j),dist(2,j),samples);
-            pnts = x_origin - x_samples;
+            x_samples = linspace(dist(1,j),dist(2,j),samples+1);
+            x_samples = x_samples(1:end-1);
+
+            % Length of each segment sampled on the ray.
+            dt = (dist(2,j)-dist(1,j)) / samples;
+
+            pnts = x_origin - x_samples - dt/2;
             pnts = [pnts' UV_rep];
-            vol_j = (dist(2,j)-dist(1,j)) * dA;
+            vol_j = dt * dA;
             X = [X; pnts]; % inefficient, yea
             vol = [vol; repelem(vol_j, samples,1)];
         end
