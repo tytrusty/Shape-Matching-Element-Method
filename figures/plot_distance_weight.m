4871dcafc27496f72caa294c44d31ff7a20a80ab
honglin-c <chl9797@live.com>
2021-01-11 19:39:46 -0500

    update distance weight visualization

--- figures/plot_distance_weight.m
+++ figures/plot_distance_weight.m
@@ -1,12 +1,16 @@
 clear;
-cut_off = 100; % threshold
-[d1, d2] = meshgrid(0:5:150, 0:5:150); 
-dist_w = @(d1, d2) max(1.0 - d1 ./ min(d2, cut_off), 0.0);
+cut_off = 50; % threshold
+[d1, total] = meshgrid(0:2:150, 0:2:150); 
+dist_w_func = @(d1, d2) (max(1.0 - d1 ./ min(d2, cut_off), 0.0)) .* (d1 <= d2);
+dist_w = dist_w_func(d1, total-d1);
+% dist_w(d1 > total - d1) = NaN;
+% dist_w(total - d1 < 0) = NaN;
 
-surf(d1, d2, dist_w(d1, d2));
-% colormap summer
+t = surf(d1, total, dist_w);
+colormap summer
 alpha(0.5)
+set(t, 'edgealpha', 0.6)
 % shading interp
 xlabel('Primary Ray Length')
-ylabel('Secondary Ray Length')
+ylabel('Total Ray Length')
 zlabel('Distance Weight')
--- figures/slice_distance_weight.m
+++ figures/slice_distance_weight.m
@@ -1,5 +1,5 @@
 clear;
-iges_file = 'mug.iges';
+iges_file = 'puft_simple.iges';
 parts = nurbs_from_iges(iges_file);
 [V, F] = triangulate_iges(parts);
 
@@ -12,7 +12,9 @@ E_sqr = [1 2; 2 3; 3 4; 4 1];
 writePOLY_triangle('./test_sqr.poly', U_sqr, E_sqr, []);
 [TV_sqr,TF_sqr,TN_sqr] = triangle('./test_sqr.poly');
 
-alpha = 1.0; % cutoff distance
+tsurf(TF_sqr, TV_sqr);
+
+alpha = 5.0; % cutoff distance
 
 step = 50; % number of the slicing steps 
 TF_list = cell(step,1);
@@ -22,20 +24,26 @@ w_list = cell(step,1);
 ii = 1;
 for d = linspace(min(V(:,2))+0.2,max(V(:,2)-0.2),step)
   plane = [0 1 0 -d]; % define the plane
-  [U,E,J] = slice_triangles(V,F,plane); % find the cross section of the nurbs
-  U_seg = [U(:,1) U(:,3)];
+  try
+    [U,E,J] = slice_triangles(V,F,plane); % find the cross section of the nurbs
+    U_seg = [U(:,1) U(:,3)];
+    U = [U_seg; U_sqr];
+    E = [E; E_sqr + repmat(max(E,[],'all'),size(E_sqr,1),size(E_sqr,2))];
+  catch
+    U = U_sqr;
+    E = E_sqr;
+  end
   
   % concat the rectange and the cross section contour
-  U = [U_seg; U_sqr];
-  E = [E; E_sqr + repmat(max(E,[],'all'),size(E_sqr,1),size(E_sqr,2))];
+
   writePOLY_triangle('./test.poly', U, E, []);
   
   % triangulate the cross section
-  [TV,TF,TN] = triangle('./test.poly','Quality',true,'MaxArea',0.1);
+  [TV,TF,TN] = triangle('./test.poly','Quality',true,'MaxArea',0.01);
     
   % compute the distance weight
   X = [TV(:,1) repmat(d,size(TV,1),1) TV(:,2)];
-  w = distance_weights(parts, X, alpha, false);
+  w = distance_weights(parts, X, alpha, true);
   
   TF_list{ii} = TF;
   X_list{ii} = X;
@@ -47,17 +55,22 @@ end
 % for visualization
 clf;
 figure(1);
-t = tsurf(F,V,'FaceColor',[0.8 0.8 0.8],'FaceAlpha',0.4,'EdgeColor','none');
-hold on;
 s = tsurf([1 1 1],V,'EdgeColor','none');
-colormap summer
+c = parula(10);
+colormap(c);
 colorbar
+shading interp;
 caxis([0 1])
 axis equal
+hold on;
+t = tsurf(F,V,'FaceColor',[0.8 0.8 0.8],'FaceAlpha',0.4,'EdgeColor','none');
 hold off;
-for ii = 1:step
-  set(s, 'Faces', TF_list{ii}, 'Vertices', X_list{ii}, 'CData', w_list{ii}(:,2), 'FaceAlpha', 0.8);
-  drawnow;
+for pid = 1:size(parts,1)
+  for ii = 1:step
+    set(s, 'Faces', TF_list{ii}, 'Vertices', X_list{ii}, 'CData', w_list{ii}(:,pid), 'FaceAlpha', 0.75);
+    drawnow;
+    pause(0.01)
+  end
 end
 
 % triangulate the nurbs parts
