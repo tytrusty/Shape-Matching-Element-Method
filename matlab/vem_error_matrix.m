3e3c5e7c68bf38bc47b658cafaca0fb9508ca593
Ty Trusty <tytrusty@utexas.edu>
2021-01-15 01:43:39 -0600

    faster error matrix woooOoOoO

--- matlab/vem_error_matrix.m
+++ matlab/vem_error_matrix.m
@@ -1,27 +1,12 @@
-function ME = vem_error_matrix(Y, W, W_S, L, w, E)
-    m = size(Y,1);
-    ME = zeros(size(L,2), size(L,2));
-    d = size(Y,2);
-    d=3;
-    n = size(w,2);
-    dk = size(Y,3);
-
+function ME = vem_error_matrix(Y, W, W_S, L, w, E, d)
+    m = size(Y,1);    
+    M1 = zeros(size(L,1),size(L,1));
+    M2 = zeros(size(L,1),size(L,2));
+    M3 = eye(size(L,2));
+    tic
     for i=1:m
-%     	Yi = squeeze(Y(i,:,:))*W{i} * W_S{i}; % weighed monomial basis
-%         for j=1:n
-%             cols=n*dk + d*(j-1)+1 : n*dk + d*j;
-%             Yi(:,cols) = eye(d)*w(i,j);
-%         end
-%         
-        I = zeros(d,size(L,2));
-        I(:, d*(i-1)+1: d*i) = eye(d);
-%         
-%         J = I - Yi*L;
-        J = I - Y{i}*L;
-        
-        % Do the multiplication in the loop without L
-        % (I-Yi)T(I-Yi) = I - two inner terms + outer term (can add these
-        % terms separately, then multiply the L later)
-        ME = ME + J'*J;
-    end     
+        M1 = M1 + Y{i}'*Y{i};
+        M2(:,d*(i-1)+1:d*i) = Y{i}';
+    end
+    ME = L'*M1*L - L'*M2 - M2'*L + M3;
 end
