304f9ee0a3af41ecd333290e51c82b81582ac782
Ty Trusty <tytrusty@utexas.edu>
2021-01-15 11:40:29 -0600

    added script to generate centers of mass

---
+++ matlab/generate_com.m
@@ -0,0 +1,31 @@
+function [x_coms, com_cluster, com_map] = generate_com(parts, x0, E, distance_cutoff, n)
+% Generates centers of mass (most of this is temporary)
+    centroids = zeros(n, 3);
+    x_coms = zeros(3,n);
+    for i=1:n
+        centroids(i,:) = mean(parts{i}.x0,2)';
+    end
+    
+    cutoff_sqr = distance_cutoff^2;
+    adj = zeros(n,n);
+    for i=1:n
+    	F = parts{i}.hires_T;
+        V = parts{i}.hires_x0';
+        sqrD = point_mesh_squared_distance(centroids,V,F);
+        adj(:,i) = sqrD < cutoff_sqr;
+    end
+    
+    adjacent = cell(n,1);
+    for i=1:n
+       	adj_idx = find(adj(i,:));
+        x_idx = horzcat(E{adj_idx});
+        adjacent{i} = x_idx(:);
+        x_coms(:,i) = mean(x0(:,adjacent{i}),2);
+    end
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%
+    [C,IA,com_map] = uniquetol(x_coms(1,:));
+    com_cluster = adjacent(IA);
+    x_coms = x_coms(:,IA);
+    
+end
