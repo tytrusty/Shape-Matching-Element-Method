5e4255b0a609c140b0220885ddc09bd33ce3d359
honglin-c <chl9797@live.com>
2021-01-24 20:07:07 -0500

    added chicken example

---
+++ examples/quadratic_chicken_with_collision.m
@@ -0,0 +1,49 @@
+function quadratic_chicken_with_collision
+
+function pinned_ids = pin_function(x)
+  pinned_ids = [];
+end
+
+iges_file = 'chicken_from_autodesk_gallery.iges';
+
+part = nurbs_from_iges(iges_file);
+
+% YM = 2e3; %in Pascals
+% pr =  0.25;
+% YM = 1e5;
+YM = 1e4;
+pr = 0.47;
+[lambda, mu] = emu_to_lame(YM, pr);
+
+options.dt=0.01;
+options.rho = 1.27e3;
+
+options.order = 2;
+options.pin_function = @pin_function;
+options.gravity = -10;
+options.enable_secondary_rays = true;
+options.lambda = lambda;
+options.mu = mu;
+% options.sample_interior = 1;
+
+options.plot_points=1;
+options.x_samples=3; %2 on regular 3 on bad
+options.y_samples=3;
+options.z_samples=3;
+% options.z_samples=11;
+
+options.distance_cutoff = 20;
+% options.distance_cutoff=0.2;
+options.save_obj = false;
+% options.k_stability = 1e7;
+
+% options.collision_ratio = 4e-1;
+options.collision_ratio = 1;
+options.collision_with_other = false;
+options.self_collision = false;
+options.collision_with_plane = true;
+options.collision_plane_z = -5;
+options.initial_velocity = [-10 0 -10];
+vem_simulate_nurbs_with_collision(part, options);
+  
+end
