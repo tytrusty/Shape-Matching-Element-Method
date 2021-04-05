aaff04f7c6db56b47a262bc685c517b0c3a1dc44
Ty Trusty <tytrusty@utexas.edu>
2021-04-05 11:31:13 -0500

    fixed stable neohookean bit

--- matlab/symbolic/neohookean_func.m
+++ matlab/symbolic/neohookean_func.m
@@ -9,16 +9,25 @@ syms C D
 % neohookean
 psi=C*(I3-3)+D*(J-1)^2;
 
-% stable neohookean
+% % stable neohookean
 % I3=det(F);
 % I2=trace(F'*F);
-% psi=C*(I2-3)-C*(I3-1)+D*(I3-1)^2 + (C/D)^2;
+% psi=C*(I2-3)-2*C*(I3-1)+D*(I3-1)^2;
+
+% SNH
+syms Mu La
+J = det(F);
+IC = trace(F'*F);
+a = 1.0 + (Mu/La) - (Mu / (4*La));
+psi = Mu*(IC-3) + La*(J-a)^2 - Mu*log(IC+1);
 
 g = gradient(psi,F(:));
 H = hessian(psi,F(:));
 % ccode(psi)
 ccode(g)
-% ccode(H)
+ccode(H)
+matlabFunction(g, 'vars', {F,Mu,La},'File','SNH_dF','Comments','Version: 1.0')
+
 % matlabFunction(g, 'vars', {F,C,D},'File','SNH_dF','Comments','Version: 1.0')
 % matlabFunction(H, 'vars', {F,C,D},'File','SNH_dF2','Comments','Version: 1.0')
 end
